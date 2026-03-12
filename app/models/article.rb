bee087edfcf90c08d6cf40bd8e82921bbb2f4a9b
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2026-03-12 11:03:51 +0500

    practice the active record basics

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,10 @@ class Article < ApplicationRecord
   include Visible
   has_many :comments
   validates :title, presence: true
+  after_create :send_notification
+
+  private
+  def send_notification
+    puts "article is created"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_03_10_081731) do
+ActiveRecord::Schema[7.2].define(version: 2026_03_11_084638) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,13 @@ ActiveRecord::Schema[7.2].define(version: 2026_03_10_081731) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+  end
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
