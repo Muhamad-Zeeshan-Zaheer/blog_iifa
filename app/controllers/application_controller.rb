34780abfd38ab4c51f093e95c1d82105f303c261
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2026-03-16 10:32:47 +0500

    resolved the authentication issues in application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,6 @@ class ApplicationController < ActionController::Base
     redirect_to new_session_path unless session.include? :user_id
   end
 
-
-
-  def require_login
-    redirect_to new_session_path unless session.include? :user_id
-  end
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
