ddcc10fbc88c4fd86e4e9cc40c11fa41f68e3884
Muhammad Zeeshan Zaheer <shan6818@gmail.com>
2026-03-24 14:35:27 +0500

    added the callbacks in article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,10 @@ class Article < ApplicationRecord
   include Visible
   has_many :comments
   validates :title, presence: true
+  before_validation :set_default_title
+  after_create -> { Rails.logger.info("New article created successfully!") }
+  private
+  def set_default_title
+    self.title = "Untitled Article"
+  end
 end
